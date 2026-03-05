be400307401c2e0adef3e9cce0736af6313cb1fd
Jullya2 <jullya.monteiro@alunos.sc.senac.br>
2026-03-05 14:11:12 -0300

    movimentação do inimigo

--- Escape/Assets/Scripts/Enemy.cs
+++ Escape/Assets/Scripts/Enemy.cs
@@ -4,6 +4,35 @@ public class Enemy : MonoBehaviour
 {
     public int life = 3;
 
+    public Transform pointA;
+    public Transform pointB;
+
+    public float speed = 2f;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = pointB;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target.position) < 0.1f)
+        {
+            if (target == pointA)
+            {
+                target = pointB;
+            }
+            else
+            {
+                target = pointA;
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Attack"))
